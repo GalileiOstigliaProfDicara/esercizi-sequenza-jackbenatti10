2f5ccd9c61a22038ca3b31023796df280e69d2ce
jackbenatti10 <giacomobenatti85@gmail.com>
2025-10-30 21:39:58 +0000

    push

--- esercizio3.c
+++ esercizio3.c
@@ -1,15 +1,18 @@
-//Chieste all'utente le dimensioni di un rettangolo, calcola e mostra area e 
-//perimetro.
+
 
 #include <stdio.h>
 
 int main() {
-    int base, altezza, area, perimetro;
-    printf("Inserisci base e altezza del rettangolo: ");
-    scanf("%d", &base);
-    scanf("%d", &altezza);
-
-    area = base * altezza;
-    perimetro = (base + altezza) * 2;
-    printf("L'area del rettangolo è %d e il perimetro è %d", area, perimetro);
-}
+    int prezzo,percentuale_sconto, sconto_effettuato, prezzo_scontato;
+    
+    printf("inserisci il prezzo:");
+    scanf("%d , &sconto_percentuale);
+        
+        sconto_effettuato = prezzo * (sconto_percentuale/100);
+        printf(" lo sconto effettuato è %d\n, sconto_effettuato");
+      
+        prezzo_scontato = prezzo - sconto_effettuato;
+        printf("il prezzo scontato è %d\, prezzo_scontato");
+        
+        printf(" il prezzo pieno è %d\n, prezzo_pieno");
+        return 0;
